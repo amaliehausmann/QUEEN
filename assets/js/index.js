52ecaf31d1f90d90ce832aabb1c1d1325f9a45a1
amaliehausmann <amaliehausmann@gmail.com>
2024-05-31 12:24:22 +0200

    sjup

--- assets/js/index.js
+++ assets/js/index.js
@@ -36,3 +36,61 @@ setInterval(nextSlide, 3000); // Change image every 3 seconds
 document.addEventListener('DOMContentLoaded', () => {
     showSlide(slideIndex);
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const fullnameInput = document.getElementById('fullname');
+    const emailInput = document.getElementById('email');
+    const numberInput = document.getElementById('number');
+    const successMessage = document.getElementById('successMessage');
+
+    fullnameInput.addEventListener('input', validateFullName);
+    emailInput.addEventListener('input', validateEmail);
+    numberInput.addEventListener('input', validatePhoneNumber);
+
+    function validateFullName() {
+        const fullname = fullnameInput.value.trim();
+        if (fullname === '') {
+            fullnameInput.classList.add('invalid');
+        } else {
+            fullnameInput.classList.remove('invalid');
+        }
+    }
+
+    function validateEmail() {
+        const email = emailInput.value.trim();
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (email === '' || !emailPattern.test(email)) {
+            emailInput.classList.add('invalid');
+        } else {
+            emailInput.classList.remove('invalid');
+        }
+    }
+
+    function validatePhoneNumber() {
+        const phoneNumber = numberInput.value.trim();
+        const phonePattern = /^[0-9]+$/;
+        if (phoneNumber === '' || !phonePattern.test(phoneNumber)) {
+            numberInput.classList.add('invalid');
+        } else {
+            numberInput.classList.remove('invalid');
+        }
+    }
+
+    window.clearForm = function() {
+        validateFullName();
+        validateEmail();
+        validatePhoneNumber();
+
+        const isValidForm = document.querySelectorAll('input.invalid, select.invalid').length === 0;
+        if (isValidForm) {
+            document.getElementById('myForm').reset();
+            successMessage.style.display = 'block';
+            setTimeout(() => {
+                successMessage.style.display = 'none';
+            }, 3000);
+        }
+    }
+});
+
+
